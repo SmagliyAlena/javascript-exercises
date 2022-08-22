ee024e2782119adbd0a18c5ef6422e385c053ea2
Alena Smagliy <smagliyalena@gmail.com>
2022-08-22 16:20:37 +0400

    01,02,03 excercise complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
+const name = ''
+
+const str = "My name is " + name + ". How are you"
+const s = `My name is ${name}. How are you`;
+
+const repeatString = function(string, num) {
+
+    //validate arguments
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    let result = '';
+    let i = 0;
+    while (i < num) {
+        result += string;
+        i++
+    }
+
+
+    return result;
 
 };
+//repeatString("hey",3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    //easy solution
+    //return str.split('').reverse().join('');
 
+    // loop solution
+    let arr = []
+    for (i = 1; i <= str.length; i++) {
+        arr.push(str[str.length - i]);
+    }
+return arr.join('')
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
